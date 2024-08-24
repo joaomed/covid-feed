dbe6bd6db756fa4a7df799747bec02715882b4fb
joaomed <joaoolimedeiros@gmail.com>
2024-08-24 08:43:02 -0300

    feat: add material ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@mui/material'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route path="/home" element={<p>Home</p>}/>
+      <Route path="/home" element={<Button>Home</Button>}/>
       <Route path="*" element={<Navigate to="/home"/>}/>
     </Routes>
   )
